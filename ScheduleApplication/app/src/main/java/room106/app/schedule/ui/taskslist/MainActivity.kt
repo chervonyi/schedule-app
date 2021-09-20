587f54d23bc1c50b4fa1cb494e909483352aca2e
chervonyi <yuri.ternopil@gmail.com>
2021-09-20 17:30:31 -0500

    Lock portrait orientation only for phones

--- ScheduleApplication/app/src/main/java/room106/app/schedule/ui/taskslist/MainActivity.kt
+++ ScheduleApplication/app/src/main/java/room106/app/schedule/ui/taskslist/MainActivity.kt
@@ -1,7 +1,9 @@
 package room106.app.schedule.ui.taskslist
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import room106.app.schedule.R
 import room106.app.schedule.ui.taskslist.fragments.TasksFragment
 
@@ -12,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Lock portrait orientation only for phones.
+        if (resources.getBoolean(R.bool.portrait_only)){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        }
 
         tasksFragment = TasksFragment()
 
